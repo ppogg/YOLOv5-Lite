aff82aa97dda3ffe3b3b871182078b8ceab885dc
pogg <82716366+ppogg@users.noreply.github.com>
2024-03-03 19:40:18 +0800

    Update test.py

--- test.py
+++ test.py
@@ -285,7 +285,7 @@ def test(data,
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='test.py')
-    parser.add_argument('--weights', nargs='+', type=str, default='weights/v5lite-s.pt', help='model.pt path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default='weights/v5Lite-s.pt', help='model.pt path(s)')
     parser.add_argument('--data', type=str, default='data/coco.yaml', help='*.data path')
     parser.add_argument('--batch-size', type=int, default=64, help='size of each image batch')
     parser.add_argument('--img-size', type=int, default=320, help='inference size (pixels)')
