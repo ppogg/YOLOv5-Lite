5d3166d2e9616866b4bfaeb121e65313d4ba563d
pogg <82716366+ppogg@users.noreply.github.com>
2024-04-06 20:12:07 +0800

    Update yolo.py

--- models/yolo.py
+++ models/yolo.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 from models.common import *
 from models.experimental import *
 from utils.autoanchor import check_anchor_order
-from utils.general import make_divisible, check_file, set_logging
+from utils.general import make_divisible, check_file, set_logging, xywh2xyxy
 from utils.torch_utils import time_synchronized, fuse_conv_and_bn, model_info, scale_img, initialize_weights, \
     select_device, copy_attr
 
